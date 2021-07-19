5d7fbb2100a8d060461709dfe246b1e7efac3247
gfmarafon <gfmarafon@gmail.com>
2021-07-19 20:18:47 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pontomais-functions',
+      version='1.0',
+      description='Functions package to interact with Pontomais APIs',
+      packages=['functions'],
+      zip_safe=False)
